4ca32255826c6b987dfa9e417839154065ed0fe6
John Cartwright <johncartwright302@gmail.com>
2017-05-15 11:55:53 +1000

    Create mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,28 @@
+/// Name of your mod
+name = "Arma 3 flood.";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "\A3\Data_F_Exp\Images\apex_overviewPicture_co.paa";
+/// Display next to the item added by the mod
+logoSmall = "\A3\ui_f\data\igui\cfg\simpleTasks\types\attack_ca.paa";
+/// Logo displayed in the main menu
+logo = "\A3\ui_f\data\igui\cfg\simpleTasks\types\attack_ca.paa";
+/// When the mouse is over, in the main menu
+logoOver = "\A3\ui_f\data\igui\cfg\simpleTasks\types\attack_ca.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "http://www.securitronlinux.com";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "Arma 3 changed for the better.";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] = 
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+/// Overview text, displayed from the extension menu
+overview = "Mod to change Arma 3 water level.";
+/// Hide the extension name
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
